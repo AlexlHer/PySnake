4a8c312eb8a8fe3a5db45a192f471aaff1cfdf7c
Alexandre l'Heritier <1499145+AlexlHer@users.noreply.github.com>
2017-05-08 02:40:40 +0200

    Version 3.0

--- PySnake.py
+++ PySnake.py
@@ -1,13 +1,52 @@
 # -*- coding: utf-8 -*-
 # ------------------------------
-# PySnake v2.0
+# PySnake v3.0
 # Auteur : Alexandre l'Heritier
 #-------------------------------
 from tkinter import *
 import random
+import time
 
 DEBUG = False
-version = "PySnake v2.0"
+version = "PySnake v3.0"
+
+def _color(self, num_couleur):
+	if num_couleur == 0:
+		self.color = "#ffffff"
+		self.background = "#000000"
+		self.cadrepcouleur = "#ffffff"
+		self.cadredcouleur = "#ffffff"
+	elif num_couleur == 1:
+		self.color = "#ff0000"
+		self.background = "#840000"
+		self.cadrepcouleur = "#ff0000"
+		self.cadredcouleur = "#ff0000"
+	elif num_couleur == 2:
+		self.color = "#5386fc"
+		self.background = "#003b9b"
+		self.cadrepcouleur = "#5386fc"
+		self.cadredcouleur = "#5386fc"
+	elif num_couleur == 3:
+		self.color = "#00ff6f"
+		self.background = "#007c36"
+		self.cadrepcouleur = "#00ff6f"
+		self.cadredcouleur = "#00ff6f"
+	elif num_couleur == 4:
+		self.color = "#000000"
+		self.background = "#ffffff"
+		self.cadrepcouleur = "#000000"
+		self.cadredcouleur = "#000000"
+	elif num_couleur == 5:
+		self.color = "#ff0000"
+		self.background = "#000000"
+		self.cadrepcouleur = "#ff0000"
+		self.cadredcouleur = "#ff0000"
+	elif num_couleur == 6:
+		self.color = "#0000ff"
+		self.background = "#000000"
+		self.cadrepcouleur = "#0000ff"
+		self.cadredcouleur = "#0000ff"
+	self.canvas.config(bg = self.background)
 
 def _dessin_line(self, posdeb, posfin, horiverti, xy):
 	tab = []
@@ -56,13 +95,9 @@ def _verif_pos(self, newposx, newposy):
 	return (newposx, newposy)
 
 def _create_grille(self):
-	self.tab_menu = []
 	self.tab_menu.append(self.canvas.create_rectangle(self.cadrepx0, self.cadrepy0, self.cadrepx1, self.cadrepy1, outline=self.cadrepcouleur, width=5))
 
 	self.tab_menu.append(self.canvas.create_rectangle(self.cadredx0, self.cadredy0, self.cadredx1, self.cadredy1, outline=self.cadredcouleur, width=1))
-
-	self.nb_ligne = 24
-	self.nb_colonne = 14
 
 	for i in range(1, self.nb_ligne+1):
 		x0temp = self.cadredx0 + ((self.cadredx1 - self.cadredx0) / (self.nb_ligne + 1)) * i
@@ -79,8 +114,7 @@ def _create_grille(self):
 		self.tab_menu.append(self.canvas.create_line(x0temp, y0temp, x1temp, y1temp, fill = self.color))
 
 def _create_menu(self):
-	self.color = "#ffffff"
-	self.canvas.config(bg = "#000000")
+	_color(self, 0)
 	self.cadrepx0 = 13
 	self.cadrepy0 = 13
 	self.cadrepx1 = self.WIDTH - 10
@@ -143,22 +177,23 @@ def _create_menu(self):
 	self.tab_menu += _dessin_line(self, 24, 40, "x", 10)[0]
 	self.tab_menu += _dessin_line(self, 10, 14, "y", 24)[0]
 	self.tab_menu += _dessin_line(self, 10, 14, "y", 40)[0]
-	self.tab_menu.append(self.canvas.create_text(900, 378, text = "Mode survie - Plusieurs niveaux", fill = "#ffffff", font=("Purisa", 20)))
+	self.tab_menu.append(self.canvas.create_text(self.WIDTH /1.333, self.HEIGHT /1.85, text = "Mode survie - Plusieurs niveaux", fill = "#ffffff", font=("Purisa", 14)))
 
 	self.tab_menu += _dessin_line(self, 24, 40, "x", 19)[0]
 	self.tab_menu += _dessin_line(self, 24, 40, "x", 15)[0]
 	self.tab_menu += _dessin_line(self, 15, 19, "y", 24)[0]
 	self.tab_menu += _dessin_line(self, 15, 19, "y", 40)[0]
-	self.tab_menu.append(self.canvas.create_text(900, 522, text = "Mode infinie - Niveau sans murs", fill = "#ffffff", font=("Purisa", 20)))
+	self.tab_menu.append(self.canvas.create_text(self.WIDTH /1.333, self.HEIGHT /1.333, text = "Mode infinie - Niveau sans murs", fill = "#ffffff", font=("Purisa", 14)))
+	self.tab_menu.append(self.canvas.create_text(self.WIDTH/2, self.HEIGHT * 1.02, text = version, fill = self.cadrepcouleur, font=("Purisa", 14)))
 
 	def clique(event):
-		if event.x < 1128 and event.x > 667 and event.y < 452 and event.y > 311:
+		if event.x < self.WIDTH /1.06 and event.x > self.WIDTH /1.8 and event.y < self.HEIGHT /1.55 and event.y > self.HEIGHT /2.25:
 			self.unbind("<Button-1>")
 			_efface_menu(self)
 			self.mode = True
 			_play(self)
 		
-		if event.x < 1128 and event.x > 667 and event.y < 595 and event.y > 452:
+		if event.x < self.WIDTH /1.06 and event.x > self.WIDTH /1.8 and event.y < self.HEIGHT /1.18 and event.y > self.HEIGHT /1.55:
 			self.unbind("<Button-1>")
 			_efface_menu(self)
 			self.mode = False
@@ -223,8 +258,12 @@ def _efface_grille(self):
 		self.canvas.delete(elems)
 	for elems in self.miam:
 		self.canvas.delete(elems)
+	self.canvas.delete(self.information[0])
+	self.canvas.delete(self.information[1])
 
 def _level_0(self):
+	self.nb_ligne = 24
+	self.nb_colonne = 14
 	_create_grille(self)
 	self.tab_serpent_pos = [[4, 2], [3, 2], [2, 2]]
 	self.tab_serpent = []
@@ -245,11 +284,17 @@ def _level_0(self):
 	self.bonbon = 0
 
 	self.time = 500
-	self.time_dim = -1
+	self.time_dim = 0
+	self.change_color = False
 
 def _level_1(self):
-	self.color = "#ffffff"
+	self.nb_ligne = 24
+	self.nb_colonne = 14
+
+	_color(self, 0)
+
 	_create_grille(self)
+
 	self.tab_serpent_pos = [[4, 2], [3, 2], [2, 2]]
 	self.tab_serpent = []
 	self.tab_serpent.append(_dessin_carre(self, 4, 2))
@@ -287,10 +332,10 @@ def _level_1(self):
 	self.time_dim = -15
 
 def _level_2(self):
-	self.color = "#ff0000"
-	self.canvas.config(bg = "#840000")
+	_color(self, 1)
 
 	_create_grille(self)
+
 	self.tab_serpent_pos = [[4, 2], [3, 2], [2, 2]]
 	self.tab_serpent = []
 	self.tab_serpent.append(_dessin_carre(self, 4, 2))
@@ -343,6 +388,9 @@ def _level_2(self):
 	self.time = 500
 	self.time_dim = -15
 
+	self.updown = 0
+	self.leftright = 1
+
 def _play(self):
 
 	global DEBUG
@@ -351,11 +399,17 @@ def _play(self):
 	self.appui_touche = True
 	self.niveau = 1
 	self.continu = True
+	self.quit = False
+	self.tab_menu = []
 
 	if self.mode:
 		_level_1(self)
 	else:
 		_level_0(self)
+
+	self.information = []
+	self.information.append(self.canvas.create_text(self.WIDTH/2, self.HEIGHT * 1.02, fill = self.cadrepcouleur, font=("Purisa", 14)))
+	self.information.append(self.canvas.create_text(self.WIDTH/2, self.HEIGHT * 1.06, text = "Appuyer sur Echap pour retourner au menu principal", fill = self.cadrepcouleur, font=("Purisa", 14)))
 
 	def clavier(event):
 		touche = event.keysym
@@ -363,29 +417,32 @@ def _play(self):
 		if DEBUG:
 			print(touche)
 
-		if touche == "Up":
+		if touche == "Up" or touche == "z":
 			if self.updown != 1 and self.leftright != 0 and self.appui_touche:
 				self.updown = -1
 				self.leftright = 0
 				self.appui_touche = False
 		
-		if touche == "Down":
+		if touche == "Down" or touche == "s":
 			if self.updown != -1 and self.leftright != 0 and self.appui_touche:
 				self.updown = 1
 				self.leftright = 0
 				self.appui_touche = False
 		
-		if touche == "Left":
+		if touche == "Left" or touche == "q":
 			if self.updown != 0 and self.leftright != 1 and self.appui_touche:
 				self.updown = 0
 				self.leftright = -1
 				self.appui_touche = False
 		
-		if touche == "Right":
+		if touche == "Right" or touche == "d":
 			if self.updown != 0 and self.leftright != -1 and self.appui_touche:
 				self.updown = 0
 				self.leftright = 1
 				self.appui_touche = False
+
+		if touche == "Escape":
+			self.quit = True
 	
 
 	def boucle():
@@ -406,13 +463,13 @@ def _play(self):
 			self.continu = False
 			self.unbind("<Key>")
 			self.rect = []
-			self.rect.append(self.canvas.create_rectangle(303, 200, 900, 503, fill = self.color))
-			self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT /3, text = "Vous avez perdu", fill = "#000000", font=("Purisa", 20)))
-			self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT /2, text = "Nb de bonbon : " + str(self.bonbon) + "    Niveau : " + str(self.niveau), fill = "#000000", font=("Purisa", 20)))
-			self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT *2/3, text = "Cliquer ici pour retourner au menu principal", fill = "#000000", font=("Purisa", 20)))
+			self.rect.append(self.canvas.create_rectangle(self.WIDTH /3.96, self.HEIGHT /3.5, self.WIDTH /1.3333, self.HEIGHT /1.39, fill = self.background))
+			self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT /3, text = "Vous avez perdu", fill = self.color, font=("Purisa", 14)))
+			self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT /2, text = "Nb de bonbon : " + str(self.bonbon) + "    Niveau : " + str(self.niveau), fill = self.cadrepcouleur, font=("Purisa", 14)))
+			self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT *2/3, text = "Cliquer ici pour retourner au menu principal", fill = self.cadrepcouleur, font=("Purisa", 14)))
 
 			def clique(event):
-				if event.x < 900 and event.x > 303 and event.y < 503 and event.y > 200:
+				if event.x < self.WIDTH /1.333 and event.x > self.WIDTH /3.96 and event.y < self.HEIGHT /1.39 and event.y > self.HEIGHT /3.5:
 					self.unbind("<Button-1>")
 					self.unbind("<Key>")
 					for elems in self.rect:
@@ -429,13 +486,54 @@ def _play(self):
 			self.bonbon = 0
 			if self.niveau == 1:
 				self.niveau += 1
+				self.information[0] = self.canvas.create_text(self.WIDTH/2, self.HEIGHT * 1.02, fill = self.cadrepcouleur, font=("Purisa", 14))
 				_level_2(self)
+			else:
+				self.continu = False
+				self.unbind("<Key>")
+				self.rect = []
+				self.rect.append(self.canvas.create_rectangle(self.WIDTH /3.96, self.HEIGHT /3.5, self.WIDTH /1.3333, self.HEIGHT /1.39, fill = self.background))
+				self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT /3, text = "Vous avez gagner !", fill = self.color, font=("Purisa", 14)))
+				self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT /2, text = "Nb de bonbon : " + str(self.bonbon) + "    Niveau : " + str(self.niveau), fill = self.cadrepcouleur, font=("Purisa", 14)))
+				self.rect.append(self.canvas.create_text(self.WIDTH /2, self.HEIGHT *2/3, text = "Cliquer ici pour retourner au menu principal", fill = self.cadrepcouleur, font=("Purisa", 14)))
+				def clique(event):
+					if event.x < self.WIDTH /1.333 and event.x > self.WIDTH /3.96 and event.y < self.HEIGHT /1.39 and event.y > self.HEIGHT /3.5:
+						self.unbind("<Button-1>")
+						self.unbind("<Key>")
+						for elems in self.rect:
+							self.canvas.delete(elems)
+						_efface_grille(self)
+						_create_menu(self)
+
+				self.bind("<Button-1>", clique)
+
+		if self.mode:
+			self.canvas.itemconfig(self.information[0], text = "Niveau " + str(self.niveau) + "			Bonbons mangés : " + str(self.bonbon) + "		Bonbon restant : " + str(self.limite_miam), fill = self.cadrepcouleur)
+			self.canvas.itemconfig(self.information[1], fill = self.cadrepcouleur)
+		else:
+			self.canvas.itemconfig(self.information[0], text = "Bonbons mangés : " + str(self.bonbon), fill = self.cadrepcouleur)
+			self.canvas.itemconfig(self.information[1], fill = self.cadrepcouleur)
+			if self.bonbon % 5 == 0:
+				if self.change_color:
+					_color(self, random.randint(0, 6))
+					self.change_color = False
+					_create_grille(self)
+					for elems in self.miam:
+						self.canvas.itemconfig(elems, fill = self.color)
+			else:
+				self.change_color = True
+		if self.quit:
+			self.continu = False
+			self.unbind("<Key>")
+			_efface_grille(self)
+			_create_menu(self)
 
 		if self.continu:
 			self.after(self.time, boucle)
 	
 	boucle()
 	self.bind("<Key>", clavier)
+
 
 class Application(Tk):
 
@@ -454,7 +552,7 @@ class Application(Tk):
 		self.WIDTH 	= 1200
 		self.HEIGHT = 700
 
-		self.canvas = Canvas(self, width = self.WIDTH, height = self.HEIGHT, bg = "#000000")
+		self.canvas = Canvas(self, width = self.WIDTH, height = self.HEIGHT+60, bg = "#000000")
 
 		_create_menu(self)
 		
@@ -466,3 +564,9 @@ def main():
 	return 0
 
 main()
+
+"""
+A faire :
+- 2 joueurs
+- Plus de levels
+"""
