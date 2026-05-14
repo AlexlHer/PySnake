80d91ff3fb3caad5003afe75130993e60c196da6
Alexandre l'Heritier <1499145+AlexlHer@users.noreply.github.com>
2026-05-14 13:48:52 +0200

    Ajout d'une version 6.0 qui trainait aussi sur mon DD

---
+++ dessins.py
@@ -0,0 +1,36 @@
+#------------------------------------------------------------------------------
+# Copyright 2017-2026 Alexandre l'Heritier
+# See the top-level LICENSE file for details.
+# SPDX-License-Identifier: GPL-3.0-or-later
+------------------------------------------------------------------------------
+
+def _dessin_line(self, posdeb, posfin, horiverti, xy):
+	tab = []
+	pos = []
+	if horiverti == "x":
+		for i in range(posdeb, posfin+1):
+			tab.append(_dessin_carre(self, i, xy))
+			pos.append([i, xy])
+
+	if horiverti == "y":
+		for i in range(posdeb, posfin+1):
+			tab.append(_dessin_carre(self, xy, i))
+			pos.append([xy, i])
+
+	return (tab, pos)
+
+def _dessin_carre(self, posx, posy):
+	self.testx0 = self.cadredx0 + ((self.cadredx1 - self.cadredx0) / (self.nb_ligne + 1)) * posx
+	self.testy0 = self.cadredy0 + ((self.cadredy1 - self.cadredy0) / (self.nb_colonne + 1)) * posy
+	self.testx1 = self.cadredx0 + ((self.cadredx1 - self.cadredx0) / (self.nb_ligne + 1)) * (posx + 1)
+	self.testy1 = self.cadredy0 + ((self.cadredy1 - self.cadredy0) / (self.nb_colonne + 1)) * (posy + 1)
+
+	return self.canvas.create_rectangle(self.testx0, self.testy0, self.testx1, self.testy1, fill = self.color)
+
+def _dessin_rond(self, posx, posy):
+	self.testx0 = self.cadredx0 + ((self.cadredx1 - self.cadredx0) / (self.nb_ligne + 1)) * posx
+	self.testy0 = self.cadredy0 + ((self.cadredy1 - self.cadredy0) / (self.nb_colonne + 1)) * posy
+	self.testx1 = self.cadredx0 + ((self.cadredx1 - self.cadredx0) / (self.nb_ligne + 1)) * (posx + 1)
+	self.testy1 = self.cadredy0 + ((self.cadredy1 - self.cadredy0) / (self.nb_colonne + 1)) * (posy + 1)
+
+	return self.canvas.create_oval(self.testx0, self.testy0, self.testx1, self.testy1, fill = self.color)
---
+++ vars.py
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------------
+# Copyright 2017-2026 Alexandre l'Heritier
+# See the top-level LICENSE file for details.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#------------------------------------------------------------------------------
+
+DEBUG = False
+DEBUG_bonbon = False
+version = "PySnake v6.0"
+
